3afaa8458d3a7f5f19c73d7514978854356928ad
Adam Wickstrom <adamwick1988@gmail.com>
2022-05-20 17:58:37 -0500

    CodeWars10

---
+++ js/Solution0010.js
@@ -0,0 +1,3 @@
+//You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
+
+//Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
--- js/solution009.js
+++ js/solution009.js
@@ -1,12 +1,30 @@
-//There was a test in your class and you passed it. Congratulations!
-//But you're an ambitious person. You want to know if you're better than the average student in your class.
+//Convert number to reversed array of digits
+//Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
 
-//You receive an array with your peers' test scores. Now calculate the average and compare your score!
+//Example:
+//348597 => [7,9,5,8,4,3]
+//0 => [0]
 
-//Return True if you're better, else False!
+function digitize(n){
+    return String().split('').map(Number).reverse()
+    
+}
 
-//Note:
-//Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
-function betterThanAverage(classPoints, yourPoints) {
-   return yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length; 
-  
+return String().split('').map(Number).reverse()
+
+function convert(x){
+    let x=
+}
+const contestants = document.querySelectorAll('contestant')
+
+Array.from(contestants).forEach(element => element.addEventListener('click', checkForRose))
+
+function digitize(n){
+    return String(n).split(' ').map(Number).reverse()
+}
+
+//Remove white spaces from strings
+
+function remove(x){
+    return x.split(" ").join('')
+}
