04567d108cb594a79ed35e8a12aafcfe524e15d4
Adam Wickstrom <adamwick1988@gmail.com>
2022-06-04 08:55:13 -0500

    Solution0021

---
+++ js/Solution0022.js
@@ -0,0 +1,7 @@
+//Return a code that returns if you will have a boy or girl
+
+function chromosomeCheck(sperm) {
+    return `Congratulations! You're going to have a ${sperm === 'XY' ? 'son' : 'daughter'}.`
+  }
+  
+  
---
+++ js/solution0021.js
@@ -0,0 +1,5 @@
+//return a function that checks if the number is even or odd, do it with truthy variables.
+
+function even_or_odd(number) {
+    return number % 2 ? "Odd" : "Even"
+  }
