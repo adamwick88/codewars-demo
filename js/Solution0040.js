1658aa4e2bd7d12ba4647d5eee87411358be82e5
Adam Wickstrom <adamwick1988@gmail.com>
2022-06-23 19:14:50 -0500

    Solution Almost Friday

---
+++ js/Solution0040.js
@@ -0,0 +1,6 @@
+/*Return a function that says hello world*/
+
+helloWorld = function() {
+    var str = "Hello World!";
+    console.log( str );
+  }
---
+++ js/Solution0042.js
@@ -0,0 +1,11 @@
+
+class Ship {
+    constructor(draft,crew){
+     this.draft = draft;
+     this.crew = crew;
+    }
+     
+     isWorthIt(){
+       return this.draft - 1.5 * this.crew > 20;
+     }
+  }
