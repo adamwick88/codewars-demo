cb772c6351b161eb4c835574d96245dc85b2e4bc
Adam Wickstrom <adamwick1988@gmail.com>
2022-08-21 12:00:27 -0500

    Updates

---
+++ js/Solution0059.js
@@ -0,0 +1,5 @@
+if a = "123" then +a === 123 .
++ by itself is "unary plus" and converts variables to a number.
+
+It's roughly just a shortcut for writing Number(a) or parseInt(a)
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Unary_plus
