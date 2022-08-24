c1466dc10be6d540cb34575f46083b9aca4af08f
Adam Wickstrom <adamwick1988@gmail.com>
2022-08-23 20:19:48 -0500

    String Day 1

---
+++ js/solution0061(String).js
@@ -0,0 +1,13 @@
+
+/*Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+*/
+
+function solution(str){
+    let stuff=str.split('').reverse().join('')
+      return stuff
+    }
+    
