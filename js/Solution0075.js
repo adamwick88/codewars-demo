9c5049560b0b50ee872881dfbe7d059fba600737
Adam Wickstrom <adamwick1988@gmail.com>
2022-09-12 17:07:04 -0500

    Arrays Day 4

--- js/Solution0075.js
+++ js/Solution0075.js
@@ -9,3 +9,5 @@ P.S. Each array includes only integer numbers. Output is a number too.
 function arrayPlusArray(arr1, arr2) {
     return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
   }
+
+  
